7714dfa8436d88e563c0872811f3d027eb3f0680
ALB3NZ0 <rmusaevdg5@gmail.com>
2024-09-22 18:28:04 +0300

    sui

---
+++ app/src/main/java/com/example/mobilki2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.mobilki2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSetAlarm, btnStartTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSetAlarm = findViewById(R.id.btnSetAlarm);
+        btnStartTimer = findViewById(R.id.btnStartTimer);
+
+        // Переход на AlarmActivity
+        btnSetAlarm.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, AlarmActivity.class);
+            startActivity(intent);
+        });
+
+        // Переход на TimerActivity
+        btnStartTimer.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, TimerActivity.class);
+            startActivity(intent);
+        });
+    }
+}
